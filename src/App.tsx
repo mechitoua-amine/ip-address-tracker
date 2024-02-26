48393e743b9210022765ca5b8545a0423154e913
mehmet_emin <mohammed.mechitoua@gmail.com>
2024-02-26 11:53:48 +0100

    setting up project with tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello, World!</h1>
+      <p>
+        IP Address Tracker Search for any IP address or domain IP Address Location Timezone UTC add
+        offset value dynamically using the API ISP
+      </p>
+    </>
+  )
+}
+
+export default App
